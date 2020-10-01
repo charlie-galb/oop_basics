57550891c443c329b8ec7dbbcdd6ef28aa0a2b87
Charlie Galbraith <galbraith.charlie@gmail.com>
2020-10-01 12:27:48 +0100

    Fill in content for add_entry and read_entry methods and add basic locking mechanism

--- secret_diary/lib/secret_diary.rb
+++ secret_diary/lib/secret_diary.rb
@@ -1,18 +1,34 @@
 class SecretDiary
-  
-  def lock
 
+  attr_reader :pages
+
+  def initialize
+
+    @pages = []
+    @lock = true
+
+  end
+
+  def lock
+    @lock = true
   end
 
   def unlock
+    @lock = false
 
   end
 
-  def add_entry
+  def add_entry (entry = gets.chomp)
+
+    return raise "LOCKED" if @lock == true
+    @pages.push(entry)
+    "Entry added"
 
   end
 
   def read_diary
+
+    @pages.each { |entry| puts entry }
 
   end
 
--- secret_diary/spec/secret_diary_spec.rb
+++ secret_diary/spec/secret_diary_spec.rb
@@ -1,17 +1,33 @@
 require "secret_diary"
 
 describe SecretDiary do
-  it "responds to the method 'lock'" do
-    expect(SecretDiary.new).to respond_to(:lock)
+
+  it "adds entry to the diary" do
+    diary = SecretDiary.new
+    diary.add_entry("I'm feeling fine")
+    expect(diary.pages).to eq(["I'm feeling fine"])
+  end
+  it "responds to the method 'add_entry'" do
+    diary = SecretDiary.new
+    expect(diary.add_entry("Hello")).to eq("Entry added")
+  end
+  it "prints a diary entry on demand" do
+    diary = SecretDiary.new
+    diary.add_entry("Hello")
+    expect { diary.read_diary }.to output("Hello\n").to_stdout
+  end
+  it "prints multiple diary entries on demand" do
+    diary = SecretDiary.new
+    diary.add_entry("Hello")
+    diary.add_entry("How are you?")
+    expect { diary.read_diary }.to output("Hello\nHow are you?\n").to_stdout
+  end
+  it "Trying to add an entry throws an error when the diary is locked" do
+    diary = SecretDiary.new
+    expect {diary.add_entry "Hello"}.to raise_error("LOCKED")
   end
   it "responds to the method 'unlock'" do
     expect(SecretDiary.new).to respond_to(:lock)
   end
-  it "responds to the method 'add_entry'" do
-    expect(SecretDiary.new).to respond_to(:lock)
-  end
-  it "responds to the method 'read_diary'" do
-    expect(SecretDiary.new).to respond_to(:lock)
-  end
-  
+
 end
